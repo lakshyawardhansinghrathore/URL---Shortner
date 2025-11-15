adb7429cc5a9e5cd3a40812d943b105381bcdd0e
Lakshyawardhansinghrathore <lakshyarathore005@gmail.com>
2025-11-15 16:47:17 +0530

    Made Schema and working for Short Url in backend

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,15 +1,38 @@
 import express from "express";
-const app = express();
 import { nanoid } from "nanoid"
+import dotenv from "dotenv";
+import connectDB from "./src/config/mongo.config.js";
+dotenv.config("./.env")
+import urlSchema from "./src/models/short_url.model.js";
+
+
+const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.post("/api/create", (req, res) => {
+app.post("/api/create", middleware, (req, res) => {
     const { url } = req.body
+    const shortUrl = nanoid(7)
+    const newUrl = new urlSchema({
+        full_url: url,
+        short_url: shortUrl
+    })
+    newUrl.save()
     res.send(nanoid(7))
 })
 
+app.get("/:id", async (req, res) => {
+    const { id } = req.params
+    const url = await urlSchema.findOne({ short_url: id })
+    if (url) {
+        res.redirect(url.full_url)
+    } else {
+        res.status(404).send("Not found")
+    }
+})
+
 app.listen(3000, () => {
+    connectDB();
     console.log("Server is running on http://localhost:3000");
 })
