136aae4bfff73982c76d6f365816689358011454
Lakshyawardhansinghrathore <lakshyarathore005@gmail.com>
2025-11-22 19:25:00 +0530

    Backend Complete with error handling

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./src/config/mongo.config.js";
 dotenv.config("./.env")
 import short_url from "./src/routes/short_url.route.js";
+import { redirectFromShortUrl } from "./src/contoller/short_url.controller.js";
 
 
 
@@ -13,16 +14,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/api/create", short_url)
+app.get("/:id", redirectFromShortUrl)
 
-// app.get("/:id", async (req, res) => {
-//     const { id } = req.params
-//     const url = await urlSchema.findOne({ short_url: id })
-//     if (url) {
-//         res.redirect(url.full_url)
-//     } else {
-//         res.status(404).send("Not found")
-//     }
-// })
+app.use(errorHandler)
 
 app.listen(3000, () => {
     connectDB();
--- BACKEND/src/contoller/short_url.controller.js
+++ BACKEND/src/contoller/short_url.controller.js
@@ -1,9 +1,28 @@
-import { createShortUrlService } from "../services/short_url_services.js"
+import { getShortUrl } from "../dao/short_url.js"
+import { createShortUrlWithoutUser, createShortUrlWithUser } from "../services/short_url.service.js"
+import wrapAsync from "../utils/tryCatchWrapper.js"
 
-export const createShortUrl = async (req, res) => {
-    console.log(req.body);
-    const { url } = req.body
-    const shortUrl = await createShortUrlService(url)
-    res.send(process.env.APP_URL + shortUrl)
+export const createShortUrl = wrapAsync(async (req, res) => {
+    const data = req.body
+    let shortUrl
+    if (req.user) {
+        shortUrl = await createShortUrlWithUser(data.url, req.user._id, data.slug)
+    } else {
+        shortUrl = await createShortUrlWithoutUser(data.url)
+    }
+    res.status(200).json({ shortUrl: process.env.APP_URL + shortUrl })
+})
 
-}
+
+export const redirectFromShortUrl = wrapAsync(async (req, res) => {
+    const { id } = req.params
+    const url = await getShortUrl(id)
+    if (!url) throw new Error("Short URL not found")
+    res.redirect(url.full_url)
+})
+
+export const createCustomShortUrl = wrapAsync(async (req, res) => {
+    const { url, slug } = req.body
+    const shortUrl = await createShortUrlWithoutUser(url, customUrl)
+    res.status(200).json({ shortUrl: process.env.APP_URL + shortUrl })
+})
